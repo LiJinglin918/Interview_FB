10c8acea42d8f3b8157459e438ae08a31f16aa14
LiJinglin918 <lijinglin918@gmail.com>
2016-11-06 04:49:47 +0800

    Update MinimumDepthofBinaryTree.java

--- MinimumDepthofBinaryTree.java
+++ MinimumDepthofBinaryTree.java
@@ -25,3 +25,32 @@ public class Solution {
             return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 }
+
+
+// iterative method
+private int findMinDepth(TreeNode root) {
+	if (root == null) {
+        return 0;
+    }
+    Queue<TreeNode> nodes = new LinkedList<>();
+    int level = 0;
+    int result = 0;
+    nodes.offer(root);
+    while (result == 0) {
+        level++;
+        int size = nodes.size();
+        for (int i = 0; i < size; i++) {
+            TreeNode node = nodes.poll();
+            if (node.left == null && node.right == null) {
+                result = level;
+            }
+            if (node.left != null) {
+                nodes.offer(node.left);
+            }
+            if (node.right != null) {
+                nodes.offer(node.right);
+            }
+        }
+    }
+    return result;
+}
