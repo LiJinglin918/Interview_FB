05b34dea22be3da458d5bd4a379b3bd8ba91073b
LiJinglin918 <lijinglin918@gmail.com>
2016-11-06 04:28:17 +0800

    Create ImplementStrstr.java

---
+++ ImplementStrstr.java
@@ -0,0 +1,38 @@
+/*
+Implement strStr().
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+public class ImplementStrStr {
+	public static void main(String[] args) {
+		System.out.println(new ImplementStrStr().strStr2("abcabcd", "bcd"));
+	}
+	public int strStr(String haystack, String needle) {
+		if (needle.length() == 0)
+			return 0;
+		if (haystack == null || needle == null || haystack.length() < needle.length())
+			return -1;
+		
+		for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+			for (int j = 0; j < needle.length(); j++) {
+				if (haystack.charAt(i + j) != needle.charAt(j))                         // haystack.charAt(i + j)
+					break;
+				if (j == needle.length() - 1)
+					return i;
+			}
+		}
+		return -1;
+	}
+	public int strStr2(String haystack, String needle) {
+		if (needle.length() == 0)
+			return 0;
+		for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+			if (haystack.charAt(i) == needle.charAt(0)) {
+				String sub = haystack.substring(i, needle.length() + i);		// subString
+				if (sub.equals(needle))
+					return i;
+			}
+		}
+		return -1;
+	}
+}
