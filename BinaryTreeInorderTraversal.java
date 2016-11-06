54f860ad63979d23375b699c4e75ce3f88b56080
LiJinglin918 <lijinglin918@gmail.com>
2016-11-06 08:34:28 +0800

    Update BinaryTreeInorderTraversal.java

--- BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal.java
@@ -1,3 +1,63 @@
+// 第一个方法是用已给的下述构造函数。第二个方法是LC原题。
+/* 
+27. Binary Tree Inorder Traversal
+class Node {Node left, Node right, Node parent}
+
+Node getNext (Node current) {
+	
+} 
+*/
+
+// Inorder --visite left -> root -> right
+// First check right child, if it's not null, then the most left child of this right child is the answer
+// Else check parent, if it is null, return null --because this is the root and it is the last node in inorder
+// if not, if node.parent.left = node (node is the left child of its parent), return parent
+// if node.parent.right = node(node is the right child of its parent), go up and 
+// search ancester if any ancester is the left child of its parent, return parent
+// else return null
+'Time complexity:O(1) in average, worst O(h), space complexity: O(1)' (BT Inorder parent)
+class BTInroder {
+    public TreeNodeWithParent findNext(TreeNodeWithParent node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.right != null) {
+            TreeNodeWithParent result = node.right;
+            while (result.left != null) {
+                result = result.left;
+            }
+            return result;
+        }
+        else if (node.parent == null) {
+            return null;
+        }
+        else if (node.parent.left == node) {
+            return node.parent;
+        }
+        else {
+            TreeNodeWithParent result = node.parent;
+            while (result.parent != null && result.parent.right == result) {
+                result = result.parent;
+            }
+            return result.parent;
+        }
+    }
+}
+
+class TreeNodeWithParent {
+    public TreeNodeWithParent parent;
+    public TreeNodeWithParent left;
+    public TreeNodeWithParent right;
+    public int val;
+    public TreeNodeWithParent(int val, TreeNodeWithParent parent) {
+        this.val = val;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+}
+
 /*
 Given a binary tree, return the inorder traversal of its nodes' values.
 For example:
