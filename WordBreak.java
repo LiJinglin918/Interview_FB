3cef055418a8c2f2d384103b3af13f0e293d79d4
LiJinglin918 <lijinglin918@gmail.com>
2016-11-06 10:12:11 +0800

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -1,3 +1,4 @@
+// 第一个方法返回Boolean，第二个返回一个值。
 /*
 Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
 For example, given
@@ -20,5 +21,39 @@ public class Solution {
             }
         }
         return index[s.length()];
+    }
+}
+
+
+// Output only one result
+// Use DFS
+// Time complexity: O(2^(n/k))
+class WordBreak {
+    public String wordBreak(Set<String> dict, String input) {
+        List<String> path = new ArrayList<>();
+        helper(path, dict, input, 0);
+        StringBuilder result = new StringBuilder();
+        for (String word : path) {
+            result.append(word + " ");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    private boolean helper(List<String> path, Set<String> dict, String input, int index) {
+        if (index == input.length()) {
+            return true;
+        }
+        for (int i = index; i < input.length(); i++) {
+            String word = input.substring(index, i + 1);
+            if (dict.contains(word)) {
+                path.add(word);
+                if (helper(path, dict, input, i + 1)) {
+                    return true;
+                }
+                path.remove(path.size() - 1);
+            }
+        }
+        return false;
     }
 }
