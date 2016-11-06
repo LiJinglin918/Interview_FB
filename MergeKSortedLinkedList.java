da9e5c2a4d0c506a7789a8ca77c991168e811003
LiJinglin918 <lijinglin918@gmail.com>
2016-11-06 10:35:48 +0800

    Update MergeKSortedLinkedList.java

--- MergeKSortedLinkedList.java
+++ MergeKSortedLinkedList.java
@@ -12,6 +12,7 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
  * }
  */
 // PQ: 一个基于优先级堆的无界优先级队列。优先级队列的元素按照其自然顺序进行排序，或者根据构造队列时提供的 Comparator 进行排序
+// follow up: 给的不是list, 而是每个list的iterator.
 
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
@@ -40,5 +41,92 @@ public class Solution {
         }
         p.next = null;
         return dummyHead.next;
+    }
+}
+
+class Merge {
+    public List<Integer> mergeTwoList(List<Integer> list1, List<Integer> list2) {
+        List<Integer> result = new ArrayList<>();
+        int index1 = 0;
+        int index2 = 0;
+        while (index1 < list1.size() && index2 < list2.size()) {
+            int number1 = list1.get(index1);
+            int number2 = list2.get(index2);
+            if (number1 <= number2) {
+                result.add(number1);
+                index1++;
+            }
+            else {
+                result.add(number2);
+                index2++;
+            }
+        }
+        while (index1 < list1.size()) {
+            result.add(list1.get(index1++));
+        }
+        while (index2 < list2.size()) {
+            result.add(list2.get(index2++));
+        }
+        return result;
+    }
+
+    public List<Integer> mergeKList(List<List<Integer>> input) {
+        PriorityQueue<Number> minNumber = new PriorityQueue<>(input.size(), new Comparator<Number>() {
+            @Override
+            public int compare(Number num1, Number num2) {
+                return num1.value - num2.value;
+            }
+        });
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i).size() == 0) {
+                continue;
+            }
+            minNumber.add(new Number(input.get(i).get(0), 0, i));
+        }
+        while (!minNumber.isEmpty()) {
+            Number min = minNumber.poll();
+            result.add(min.value);
+            if (min.index < input.get(min.listIndex).size() - 1) {
+                minNumber.add(new Number(input.get(min.listIndex).get(min.index + 1), min.index + 1, min.listIndex));
+            }
+        }
+        return result;
+
+    }
+
+    class Number {
+        int value;
+        int index;
+        int listIndex;
+        public Number(int value, int index, int listIndex) {
+            this.value = value;
+            this.index = index;
+            this.listIndex = listIndex;
+        }
+    }
+
+	// 传入的是每个list的iterator
+    public List<Integer> mergeKListWithIterator(List<Iterator<Integer>> input) {
+        PriorityQueue<Number> minNumber = new PriorityQueue<>(input.size(), new Comparator<Number>() {
+            @Override
+            public int compare(Number num1, Number num2) {
+                return num1.value - num2.value;
+            }
+        });
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i).hasNext()) {
+                minNumber.add(new Number(input.get(i).next(), 0, i));
+            }
+        }
+        while (!minNumber.isEmpty()) {
+            Number min = minNumber.poll();
+            if (input.get(min.listIndex).hasNext()) {
+                minNumber.add(new Number(input.get(min.listIndex).next(), 0, min.listIndex));
+            }
+            result.add(min.value);
+        }
+        return result;
     }
 }
