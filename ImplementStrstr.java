0689fe36ed0dc42d972191ee5ecf120e0395a199
LiJinglin918 <lijinglin918@gmail.com>
2016-11-06 08:12:55 +0800

    Update ImplementStrstr.java

--- ImplementStrstr.java
+++ ImplementStrstr.java
@@ -2,7 +2,7 @@
 Implement strStr().
 Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 */
-
+// 先是用俩指针，然后用substring，然后改变为2d Array的haystack.
 public class ImplementStrStr {
 	public static void main(String[] args) {
 		System.out.println(new ImplementStrStr().strStr2("abcabcd", "bcd"));
@@ -34,5 +34,32 @@ public class ImplementStrStr {
 			}
 		}
 		return -1;
+	}
+}
+
+// 2d array 的 haystack
+public class Solution {
+	public int strStr(List<List<Character>> haystack, String needle) {
+	    int total = 0;
+	    for (int row = 0; row < haystack.size(); row++) {
+	        for (int col = 0; col < haystack.get(row).size(); col++) {
+	            int k = 0;
+	            int posRow = row;
+	            int posCol = col;
+	            while ((posRow < haystack.size() && posCol < haystack.get(posRow).size()) && k < needle.length() && haystack.get(posRow).get(posCol) == needle.charAt(k)) {
+	                k++;
+	                posCol++;
+	                if (posCol == haystack.get(posRow).size()) {
+	                    posCol = 0;
+	                    posRow++;
+	                }
+	            }
+	            if (k == needle.length()) {
+	                return total + col - 1;
+	            }
+	        }
+	        total += haystack.get(row).size();
+	    }
+	    return -1;
 	}
 }
