c82ee25a9ef386a3b04f8a67f7b82243ea3cce43
LiJinglin918 <lijinglin918@gmail.com>
2016-11-08 01:26:27 +0700

    Update Subtree.java

--- Subtree.java
+++ Subtree.java
@@ -1,36 +1,55 @@
 // 91. find if a tree is subtree of another
 // 'Time complexity: O(nk) -k nodes in subtree'
 
-class FindSubTree {
-    public boolean isValid(TreeNode root, TreeNode node) {
-        if (root == null && node == null) {
-            return true;
-        }
-        boolean result = false;
-        if (root != null && node != null) {
-            if (root.val == node.val) {
-                result = helper(root, node);
-            }
-            if (!result) {
-                result = isValid(root.right, node);
-            }
-            if (!result) {
-                result = isValid(root.left, node);
-            }
-        }
-        return result;
-    }
 
-    private boolean helper(TreeNode root, TreeNode node) {
-        if (root == null && node == null) {
-            return true;
-        }
-        if ((root == null && node != null) || (root != null && node == null)) {
-            return false;
-        }
-        if (root.val == node.val) {
-            return helper(root.right, node.right) && helper(root.left, node.left);
-        }
-        return false;
-    }
+
+
+
+/**
+ * 题意是check第二个tree是不是第一个tree的subtree
+ * 1. recursive
+ * 2. note the two identical tree are also true
+ * 3. node the null tree are also true.
+ * 4. Time complexity: O(nk) -k nodes in subtree
+ *
+ */
+public class SubtreeCheck {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode (int x) {
+			val = x;
+		}
+	}
+	public boolean subtree(TreeNode root, TreeNode node) {
+		if (root == null && node == null)
+			return true;
+		boolean res = false;
+		if (root != null && node != null) {
+			if (root.val == node.val) {				// check if they are identical. 
+				res = helper(root, node);
+			}
+			if (!res) {
+				res = subtree(root.right, node);
+			}
+			if (!res) {
+				res = subtree(root.left, node);
+			}
+		}
+		return res;
+	}
+	
+	// helper method is to check whether these two tree are identical. 
+	public boolean helper(TreeNode root, TreeNode node) {
+		if (root == null && node == null) {
+			return true;
+		}
+		if ((root == null && node != null) || (root != null && node == null))
+			return false;
+		if (root.val == node.val) {
+			return helper(root.right, node.right) && helper(root.left, node.left);
+		}
+		return false;
+	}
 }
