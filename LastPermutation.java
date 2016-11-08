af4f149e1f22ec3d477bace9b2b8edbe905135e2
LiJinglin918 <lijinglin918@gmail.com>
2016-11-08 11:20:47 +0700

    Update LastPermutation.java

--- LastPermutation.java
+++ LastPermutation.java
@@ -7,39 +7,65 @@
 // Exp: 5, 4, 1, 2, 3 previous -> 5, 3, 4, 2, 1 
 // the num[first] = 4, nums[second] = 1, nums[smaller] = 3 
 
-class PreviousPermutation {
-    public int[] previous(int[] input) {
-        if (input.length <= 1) {
-            return input;
-        }
-        int first = 0;
-        int second = 0;
-        for (int i = input.length - 1; i >= 1; i--) {
-            if (input[i] < input[i - 1]) {
-                first = i - 1;
-                second = i;
-                break;
-            }
-        }
-        int smaller = 0;
-        for (int i = input.length - 1; i >= second; i--) {
-            if (input[i] < input[first]) {
-                smaller = i;
-                break;
-            }
-        }
-        swap(input, first, smaller);
-        reverse(input, second, input.length - 1);
-        return input;
-    }
-    private void swap(int[] input, int index1, int index2) {
-        int temp = input[index1];
-        input[index1] = input[index2];
-        input[index2] = temp;
-    }
-    private void reverse(int[] input, int start, int end) {
-        while (start <= end) {
-            swap(input, start++, end--);
-        }
-    }
+
+
+
+
+
+
+
+
+/*
+ * 例子： 5,4,1,2,3 -> 5,3,4,2,1
+ * 1. from the end to the front of the array util is increasing. 
+ * 2. use variable "first" and "second" to store these two number
+ * (例子： nums[first] = 4, nums[second] = 1)
+ * 3. from the end to the front, find the smaller integer than "first"
+ * (例子： nums[smaller] = 3)
+ * 4. swap the first and smaller
+ * 5. from the second to the end, reverse the array
+ */
+
+public class LastPermutation {
+	public int[] previous(int[] input) {
+		if (input.length <= 1)
+			return input;
+		int first = 0;
+		int second = 0;
+		
+		// from the end to front, find the element that become increase
+		// store the two elements
+		for (int i = input.length - 1; i >= 1; i--) {
+			if (input[i] < input[i - 1]) {
+				first = i - 1;
+				second = i;
+				break;
+			}
+		}
+		
+		// from the element smaller than the first
+		int smaller = 0;
+		for (int i = input.length - 1; i >= second; i--) {
+			if (input[i] < input[first]) {
+				smaller = i;
+				break;
+			}
+		}
+		
+		// swap the first and smaller
+		// from the second to the end, reverse the array
+		swap(input, first, smaller);
+		reverse(input, second, input.length - 1);
+		return input;
+	}
+	public void swap(int[] input, int index1, int index2) {
+		int temp = input[index1];
+		input[index1] = input[index2];
+		input[index2] = temp;
+	}
+	public void reverse(int[] input, int start, int end) {
+		while (start <= end) {
+			swap(input, start++, end--);
+		}
+	}
 }
