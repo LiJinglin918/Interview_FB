9c72bb5dd0367df66a69c55ccffadcfab41e00d3
LiJinglin918 <lijinglin918@gmail.com>
2016-11-08 11:01:04 +0700

    Update WalkMatrixPrint.java

--- WalkMatrixPrint.java
+++ WalkMatrixPrint.java
@@ -10,9 +10,13 @@
 class WalkMatrix {
     public void print(int[][] matrix) {
         int height = matrix.length;
+        
+        // 打印对角线上部分： 1， 42， 753
         for (int i = 0; i < matrix.length; i++) {
             helper(i, 0, matrix);
         }
+        
+        // 打印对角线下部分：  86， 9
         for (int i = 1; i < matrix[0].length; i++) {
             helper(height - 1, i, matrix);
         }
