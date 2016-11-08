d93bf8b240d59e00fae9bee3400d81c6b8978c04
LiJinglin918 <lijinglin918@gmail.com>
2016-11-08 09:44:29 +0700

    Update MeetingRoomsIandII.java

--- MeetingRoomsIandII.java
+++ MeetingRoomsIandII.java
@@ -12,23 +12,45 @@ For example, Given [[0, 30],[5, 10],[15, 20]], return false.
 有冲突的定义是开始时间小于之前最晚的结束时间。这里之前最晚的结束时间不一定是上一个的结束时间，所以我们更新的时候要取最大值。
 */
 
-public class Solution {
-    public boolean canAttendMeetings(Interval[] intervals) {
-        if(intervals == null || intervals.length == 0) return true;
-        Arrays.sort(intervals, new Comparator<Interval>(){
-            public int compare(Interval i1, Interval i2){
-                return i1.start - i2.start;
-            }
-        });
-        int end = intervals[0].end;
-        // 检查每一个Interval
-        for(int i = 1; i < intervals.length; i++){
-            // 如果Interval的开始时间小于之前最晚的结束时间，就返回假
-            if(intervals[i].start < end) return false;
-            end = Math.max(end, intervals[i].end);
-        }
-        return true;
-    }
+
+
+
+
+
+
+/*
+ * 1. sort the intervals according to the start
+ * 2. check each interval.end
+ * 3. use the variable "end" to store the greatest end of intervals
+ * 4. if some intervals.start < the variable "end", false
+ */
+import java.util.*;
+public class MeetingRoomsI {
+	public boolean canAttendMeetings(Interval[] intervals) {
+		if (intervals == null || intervals.length == 0)
+			return true;
+		Arrays.sort(intervals, new Comparator<Interval>() {
+			public int compare(Interval o1, Interval o2) {
+				return o1.start - o2.start;
+			}
+		});
+		int end = intervals[0].end;
+		for (int i = 1; i < intervals.length; i++) {
+			if (intervals[i].start < end)
+				return false;
+			end = Math.max(end, intervals[i].end);
+		}
+		return true;
+	}
+	
+	public class Interval {
+		int start;
+		int end;
+		Interval (int s, int e) {
+			start = s;
+			end = e;
+		}
+	}
 }
 
 /*
