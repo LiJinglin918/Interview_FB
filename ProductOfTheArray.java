194bc4f684cad6c8ab2d4fb0d15f9fb8df4a42ee
LiJinglin918 <lijinglin918@gmail.com>
2016-11-10 23:57:30 +0700

    Update ProductOfTheArray.java

--- ProductOfTheArray.java
+++ ProductOfTheArray.java
@@ -1,30 +1,58 @@
 // 25. Product of the array
 // 给定一个array, 返回里面元素乘积的所有可能值。例如给定array:[1,2,3,4]   应该返回 [1, 2, 3, 4, 6, 8, 12, 24]
-class Product {
-    public List<Integer> product(int[] input) {
-        Set<Integer> set = new HashSet<>();
-        Arrays.sort(input);
-        helper(set, 1, 0, input, true);
-        return new ArrayList<Integer>(set);
-    }
 
-    private void helper(Set<Integer> result, int curProduct, int pos, int[] input, boolean first) {
-        if (!first) {
-            result.add(curProduct);
-        }
-        first = false;
-        for (int i = pos; i < input.length; i++) {
-            if (i != pos && input[i] == input[i - 1]) {
-                continue;
-            }
-            if (input[i] == 0) {
-                result.add(0);
-                continue;
-            }
-            curProduct *= input[i];
-            helper(result, curProduct, i + 1, input, first);
-            curProduct /= input[i];
 
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+/*
+ * backtracking. 
+ * 1. use a flag to indicate the first element
+ * 2. consider the replicates
+ * 3. If one of the elements is 0, all 0. return. Find the next index
+ */
+
+import java.util.*;
+public class ProductOfTheArray {
+	public List<Integer> product(int[] input) {
+		List<Integer> res = new ArrayList<>();
+		Arrays.sort(input);
+		helper(res, 1, 0, input, true);
+		return res;
+	}
+
+	public void helper(List<Integer> res, int curProduct, int pos, int[] input, boolean flag) {
+		
+		// use a flag to represent the first element or not
+		if (!flag) {
+			res.add(curProduct);
+		}
+		flag = false;
+		
+		// traverse the remaining input
+		for (int i = pos; i < input.length; i++) {
+			
+			// consider the replicates
+			if (i != pos & input[i] == input[i - 1])
+				continue;
+			
+			// consider one of the elements is 0, just return
+			if (input[i] == 0) {
+				res.add(0);
+				continue;
+			}
+			
+			// backtracking
+			curProduct *= input[i];
+			helper(res, curProduct, i + 1, input, flag);
+			curProduct /= input[i];
+		}
+	}
 }
