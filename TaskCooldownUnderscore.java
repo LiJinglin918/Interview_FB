ee2be816604847c774e558018e05119dd5d7bc5f
LiJinglin918 <lijinglin918@gmail.com>
2016-11-10 08:05:51 +0700

    Update TaskCooldownUnderscore.java

--- TaskCooldownUnderscore.java
+++ TaskCooldownUnderscore.java
@@ -16,8 +16,7 @@
 # Recovery interval (cooldown): 6
 # Output: 18  (1 2 3 4 5 6 _ _ 2 _ 4 _ 6 1 _ 2 _ 4)
 */ 
-http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=188881&extra=page%3D2%26filter%3Dsortid%26sortid%3D311%26searchoption%5B3090%5D%5Bvalue%5D%3D1%26searchoption%5B3090%5D%5Btype%5D%3Dradio%26searchoption%5B3046%5D%5Bvalue%5D%3D2%26searchoption%5B3046%5D%5Btype%5D%3Dradio%26sortid%3D311
-*/
+
 
 	
 	
