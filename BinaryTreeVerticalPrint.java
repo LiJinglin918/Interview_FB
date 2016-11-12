68babf224aed43d3996b4c8a24fdfdf9cb573232
LiJinglin918 <lijinglin918@gmail.com>
2016-11-13 05:01:08 +0700

    Update BinaryTreeVerticalPrint.java

--- BinaryTreeVerticalPrint.java
+++ BinaryTreeVerticalPrint.java
@@ -10,7 +10,7 @@
 
 
 
-
+// not recursive
 /*
  * 1. use a hashmap to map the col index to the integer value of root
  * 2. another hashmap to map the node to its col index. root's col index = 0; left branch is negative, right is positive
@@ -82,4 +82,51 @@ public class PrintBinaryTreeVertical {
 			val = x;
 		}
 	}
+}
+
+
+/*=========================================================================================================*/
+// recursive
+
+
+
+class TreeNode{
+	int data;
+	TreeNode left, right;
+	TreeNode(int item) {
+        data = item;
+        left = right = null;
+    }
+} 
+class Values {
+    int max, min;
+}
+public class PrintNodeVertical {
+    static TreeNode root;
+    Values val = new Values();
+    public void findMinMax(TreeNode node, Values min, Values max, int hd) {
+        if (node == null) 
+	        return;
+        if (hd < min.min) 
+            min.min = hd;
+        else if (hd > max.max) 
+            max.max = hd;
+        findMinMax(node.left, min, max, hd - 1);
+        findMinMax(node.right, min, max, hd + 1);
+    }
+    public void printVerticalLine(TreeNode node, int line_no, int hd) {
+        if (node == null) 
+            return;
+        if (hd == line_no) 
+            System.out.print(node.data + " ");        
+        printVerticalLine(node.left, line_no, hd - 1);
+        printVerticalLine(node.right, line_no, hd + 1);
+    }
+    public void verticalOrder(TreeNode node) {
+        findMinMax(node, val, val, 0);
+        for (int line_no = val.min; line_no <= val.max; line_no++) {
+            printVerticalLine(node, line_no, 0);
+            System.out.println("");
+        }
+    }
 }
