8e31001eae36ff757ea4b2bbe8416a0ddef99c5c
LiJinglin918 <lijinglin918@gmail.com>
2016-12-21 05:25:58 +0700

    Update FindThief.java

--- FindThief.java
+++ FindThief.java
@@ -9,9 +9,43 @@
 */
 
 // http://www.mitbbs.com/article_t/JobHunting/32978937.html
+// https://gist.github.com/gcrfelix/19e0dd64a7f8e9192158
+
 import java.util.Arrays;
 
 public class FindTheafInRooms {
+	
+	public boolean alibaba(int numCaves, int[] strategy) {
+		int days = strategy.length;
+		boolean[][] res = new boolean[numCaves][days];
+		for (int i = 0; i < days; i++) {
+			res[strategy[i]][i] = true;
+		}
+		for (int i = 0; i < numCaves; i++) {
+			for (int j = 1; j < days; j++) {
+				if (i == numCaves - 1) {
+					res[i][j] = res[i][j] || res[i - 1][j - 1];
+				}
+				else if (i == 0) {
+					res[i][j] = res[i][j] || res[i + 1][j - 1];
+				}
+				else {
+					res[i][j] = res[i][j] || res[i - 1][j - 1] && res[i + 1][j - 1];
+				}
+			}
+		}
+		boolean result = true;
+		for (int i = 0; i < numCaves; i++) {
+			result = result && res[i][days - 1];
+		}
+		return result;
+	}
+	
+	
+/*===========================================================================================*/
+	
+	
+	
 	public static void main(String[] args) {
 		int[] strategy = {2,2};
 		if (new FindTheafInRooms().findTheaf(4, strategy))
