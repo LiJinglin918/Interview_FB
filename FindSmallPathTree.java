664da6ae9d53596a54dfe24bb733d721a75de02f
LiJinglin918 <lijinglin918@gmail.com>
2016-12-21 12:28:30 +0700

    Update FindSmallPathTree.java

--- FindSmallPathTree.java
+++ FindSmallPathTree.java
@@ -1,4 +1,4 @@
-// 75. FindSmallPathTree
+// 75. FindSmallPathTree		
 /*
 第二道题是 给个Tree 不一定是平衡的， 要求 把所有路径排序后  按字符串那样的比较大小方法 找出最小的路径  时间要求线性的。 比如  
          5
@@ -10,41 +10,55 @@
 排序后  1 5 10 ； 5 7 10 ； 3 5 8；
 所以按字符串类型排序 为  1 5 10 < 3 5 8 < 5 7 10；
 */
-class FindSmallPath {
-    public List<Integer> find(TreeNode root) {
-        Result result = helper(root);
-        return result.path;
-    }
 
-    private Result helper(TreeNode root) {
-        if (root == null) {
-            Result result = new Result();
-            result.path.add(Integer.MAX_VALUE);
-            return result;
-        }
-        if (root.left == null && root.right == null) {
-            Result result = new Result();
-            result.min = root.val;
-            result.path.add(root.val);
-            return result;
-        }
-        Result left = helper(root.left);
-        Result right = helper(root.right);
-        Result result = new Result();
-        result.path.add(root.val);
-        if (left.min > right.min) {
-            result.path.addAll(right.path);
-        }
-        else {
-            result.path.addAll(left.path);
-        }
-        result.min = Math.min(root.val, Math.min(left.min, right.min));
-        return result;
-    }
-    class Result {
-        public int min = Integer.MAX_VALUE;
-        public List<Integer> path = new ArrayList<>();
-        public Result() {
-        }
-    }
+import java.util.*;
+public class FindSmallPathTree {
+	public List<Integer> findSmallPath(TreeNode root) {
+		Result res = helper(root);
+		Collections.sort(res.path);
+		return res.path;
+	}
+	
+	public Result helper(TreeNode root) {
+		if (root == null) {
+			
+			// new 一个，然后填属性
+			Result res = new Result();
+			res.path.add(null);
+			return res;
+		}
+		if (root.left == null && root.right == null) {
+			
+			// new 一个，然后填属性。
+			Result res = new Result();
+			res.min = root.val;
+			res.path.add(root.val);
+			return res;
+		}
+		
+		// new 一个，然后填属性。先不管path是否排序，先添加再说。
+		Result res = new Result();
+		res.path.add(root.val);
+		
+		Result left = helper(root.left);
+		Result right = helper(root.right);
+		
+		// 把左右子树含有最小的元素的那一部分path再添加进来。
+		if (left.min > right.min) {
+			res.path.addAll(right.path);
+		}
+		else {
+			res.path.addAll(left.path);
+		}
+		
+		// 最后min属性
+		res.min = Math.min(root.val, Math.min(left.min, right.min));
+		return res;
+	}
+	
+	// 属性是最小值，和路径。
+	public class Result {
+		int min = Integer.MAX_VALUE;
+		List<Integer> path = new ArrayList<>();
+	}
 }
